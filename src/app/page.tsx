ebb769418240abc9a3f1cfbbd43f04215240178a
AnkushSarkar10 <ankush.sarkar2002@gmail.com>
2025-08-29 23:05:37 -0400

    embla rough draft

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import Footer from "~/section/Footer";
 
 export default function HomePage() {
   return (
-    <main className="flex h-full w-full flex-col gap-24 overflow-hidden">
+    <main className="flex min-h-screen w-full flex-col gap-24 overflow-hidden">
+      <div className="flex-1"/>
       <Footer />
     </main>
   );
---
+++ src/components/CarouselItem.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Image from "next/image";
+
+export type CarouselItemProps = {
+  src: string;
+  fullName: string;
+  isActive: boolean;
+};
+
+export default function CarouselItem({ src, isActive, fullName }: CarouselItemProps) {
+  return (
+    <div className="flex-shrink-0">
+      <Image
+        src={src}
+        alt={fullName}
+        width={64}
+        height={64}
+        className="h-24 w-24 rounded-full object-cover md:h-28 md:w-28"
+        priority={false}
+      />
+    </div>
+  );
+}
