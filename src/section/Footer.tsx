62d233826166aa2a4a1fa64effcfc1782239b61b
AnkushSarkar10 <ankush.sarkar2002@gmail.com>
2025-09-22 21:07:19 -0400

    remove shadcn seperator

--- src/section/Footer.tsx
+++ src/section/Footer.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Carousel from "~/components/TeamCarousel";
-import { Separator } from "~/components/ui/separator";
+
 export default function Footer() {
   return (
     <div className="flex flex-col items-center justify-center bg-[#FFF7E3] py-10 text-[#766060]">
@@ -9,7 +9,6 @@ export default function Footer() {
       </p>
       <Carousel />
       <div className="flex w-full flex-col items-stretch justify-center px-3.5 md:px-44">
-        <Separator className="bg-black" />
         <div className="flex w-full flex-col items-center gap-2 pt-6 md:flex-row md:justify-between md:gap-0">
           <div className="flex flex-wrap items-center justify-between gap-5 text-xs font-normal md:justify-start md:gap-6 md:text-sm">
             <Link href="">Code of Conduct</Link>
