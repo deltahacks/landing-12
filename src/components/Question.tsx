14290ae6e2eed51e9c736876d1531f82b42098b4
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2025-10-16 17:41:10 -0700

    fix: make FAQ text bigger

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -30,7 +30,7 @@ const Question: React.FC<QuestionProps> = ({
         ref={detailsRef}
         className={clsx("group py-1.5 md:py-1.5 lg:py-2", isOpen && "open")}
       >
-        <summary className="focus-visible:ring-opacity-75 text-brown flex w-full cursor-pointer list-none items-center gap-1.5 rounded-lg px-2.5 py-1 text-left text-xs leading-tight font-medium transition-all duration-200 hover:bg-white/20 focus:outline-none focus-visible:ring focus-visible:ring-white/50 md:py-1 md:text-xs md:leading-tight lg:py-1.5 lg:text-sm lg:leading-normal">
+        <summary className="focus-visible:ring-opacity-75 text-brown flex w-full cursor-pointer list-none items-center gap-1.5 rounded-lg px-2.5 py-1 text-left text-sm leading-tight font-medium transition-all duration-200 hover:bg-white/20 focus:outline-none focus-visible:ring focus-visible:ring-white/50 md:py-1 md:text-base md:leading-tight lg:py-1.5 lg:leading-normal">
           <span className="relative h-3 w-3 flex-shrink-0">
             <PlusIcon
               className={clsx(
@@ -47,7 +47,7 @@ const Question: React.FC<QuestionProps> = ({
           </span>
           {question}
         </summary>
-        <div className="content text-brown w-full rounded-b-lg py-0.5 pl-7.5 text-xs leading-tight md:py-0.5 md:leading-tight lg:py-1 lg:leading-snug">
+        <div className="content text-brown w-full rounded-b-lg py-0.5 pl-7.5 text-sm leading-tight md:py-0.5 md:text-base md:leading-tight lg:py-1 lg:leading-snug">
           {answer}
         </div>
       </details>
